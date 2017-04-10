a3fac60d48f16b8c4180d3f5fd25f5bdc41e3927
piotrszukala <piotr_szukala@o2.pl>
2017-04-10 22:47:05 +0200

    work toggle sidebar from parent component

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,15 +1,19 @@
-import { Component, AnimationTransitionEvent } from '@angular/core';
+import { Component, AnimationTransitionEvent, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'side-bar',
   template: require('./side-bar.html')
 })
 export class SidebarComponent {
+
+  constructor (
+    private changeDetection: ChangeDetectorRef
+  ) {}
   private _opened: boolean = false;
   private _modeNum: number = 0;
   private _positionNum: number = 0;
-  private _closeOnClickOutside: boolean = true;
-  private _showBackdrop: boolean = true;
+  private _closeOnClickOutside: boolean = false;
+  private _showBackdrop: boolean = false;
   private _animate: boolean = true;
   private _trapFocus: boolean = true;
   private _autoFocus: boolean = true;
@@ -20,56 +24,9 @@ export class SidebarComponent {
   private _MODES: Array<string> = ['over', 'push', 'dock'];
   private _POSITIONS: Array<string> = ['left', 'right', 'top', 'bottom'];
 
-  private _toggleOpened(): void {
+  toggleOpened(): void {
     this._opened = !this._opened;
-  }
-
-  private _toggleMode(): void {
-    this._modeNum++;
-
-    if (this._modeNum === this._MODES.length) {
-      this._modeNum = 0;
-    }
-  }
-
-  private _toggleAutoCollapseHeight(): void {
-    this._autoCollapseHeight = this._autoCollapseHeight ? null : 500;
-  }
-
-  private _toggleAutoCollapseWidth(): void {
-    this._autoCollapseWidth = this._autoCollapseWidth ? null : 500;
-  }
-
-  private _togglePosition(): void {
-    this._positionNum++;
-
-    if (this._positionNum === this._POSITIONS.length) {
-      this._positionNum = 0;
-    }
-  }
-
-  private _toggleCloseOnClickOutside(): void {
-    this._closeOnClickOutside = !this._closeOnClickOutside;
-  }
-
-  private _toggleShowBackdrop(): void {
-    this._showBackdrop = !this._showBackdrop;
-  }
-
-  private _toggleAnimate(): void {
-    this._animate = !this._animate;
-  }
-
-  private _toggleTrapFocus(): void {
-    this._trapFocus = !this._trapFocus;
-  }
-
-  private _toggleAutoFocus(): void {
-    this._autoFocus = !this._autoFocus;
-  }
-
-  private _toggleKeyClose(): void {
-    this._keyClose = !this._keyClose;
+    this.changeDetection.detectChanges();
   }
 
   private _onOpenStart(): void {
--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { MapService } from '../../services';
-import { MarkerComponent, SearchComponent } from '../../components';
+import { MarkerComponent, SearchComponent, SidebarComponent } from '../../components';
 
 // Change the component because we want to store data in state not in the component directly
 import { Store } from '../../store';
@@ -11,47 +11,46 @@ import { Store } from '../../store';
     require('./style.scss'),
     require('leaflet/dist/leaflet.css')
   ],
-  template: `
-    <div class="top-map-menu">
-      
-      <marker></marker>
-      <side-bar></side-bar>
-    </div>  
-    <div id="map"></div>
-  `
+  template: require('./map.html')
 })
 export class MapContainer {
+  private _opened: boolean = false;
   @ViewChild(MarkerComponent) markerComponent: MarkerComponent;
   @ViewChild(SearchComponent) searchComponent: SearchComponent;
+  @ViewChild(SidebarComponent) sidebarComponent: SidebarComponent;
 
- constructor(
-   private mapService: MapService
- ) {}
+  constructor(
+    private mapService: MapService
+  ) { }
 
-ngOnInit() {
-        let map = L.map("map", {
-            zoomControl: false,
-            center: L.latLng(40.731253, -73.996139),
-            zoom: 12,
-            minZoom: 4,
-            maxZoom: 19,
-            layers: [this.mapService.baseMaps.CartoDB]
-        });
+  ngOnInit() {
+    let map = L.map("map", {
+      zoomControl: false,
+      center: L.latLng(40.731253, -73.996139),
+      zoom: 12,
+      minZoom: 4,
+      maxZoom: 19,
+      layers: [this.mapService.baseMaps.CartoDB]
+    });
 
-        L.control.zoom({ position: "topright" }).addTo(map);
-        L.control.layers(this.mapService.baseMaps).addTo(map);
-        L.control.scale().addTo(map);
+    L.control.zoom({ position: "topright" }).addTo(map);
+    L.control.layers(this.mapService.baseMaps).addTo(map);
+    L.control.scale().addTo(map);
 
-        this.mapService.map = map;
+    this.mapService.map = map;
 
-        // this.geocoder.getCurrentLocation()
-        //     .subscribe(
-        //         location => map.panTo([location.latitude, location.longitude]),
-        //         err => console.error(err)
-        //     );
+    // this.geocoder.getCurrentLocation()
+    //     .subscribe(
+    //         location => map.panTo([location.latitude, location.longitude]),
+    //         err => console.error(err)
+    //     );
 
-    }
-      ngAfterViewInit() {
-        this.markerComponent.Initialize();
-    }
+  }
+  ngAfterViewInit() {
+    this.markerComponent.Initialize();
+  }
+
+  togglePanelOpened() {
+    this.sidebarComponent.toggleOpened();
+  }
 }
